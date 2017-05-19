a49fe401fd8ff47d991c67385fde716e3c79ee97
Irene Cortinovis <irenecortinovis1@gmail.com>
2017-05-19 11:52:27 +0200

    [wip] reaction term

---
+++ fem-react-DN/a.m
@@ -0,0 +1,4 @@
+function y = a(x)
+    y = (sin(x))^2;
+
+end
---
+++ fem-react-DN/f.m
@@ -0,0 +1,5 @@
+function y = f(x)
+
+y = 2*x*(5*sin(5*x) - 1/(x + 1)) + sin(x)^2*(cos(5*x) + log(x + 1)) + (25*cos(5*x) + 1/(x + 1)^2)*(x^2 + 1);
+
+end
